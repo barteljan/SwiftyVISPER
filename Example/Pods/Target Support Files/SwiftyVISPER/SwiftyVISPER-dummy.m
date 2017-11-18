33df9cc5e6263996c88e4cc0bae75b1e68d4bee4
Jan Bartel <jan.bartel@atino.net>
2017-11-18 10:55:40 +0100

    added some documentation, changed some protocols

---
+++ Example/Pods/Target Support Files/SwiftyVISPER/SwiftyVISPER-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SwiftyVISPER : NSObject
+@end
+@implementation PodsDummy_SwiftyVISPER
+@end
---
+++ Example/Pods/Target Support Files/VISPER-Redux/VISPER-Redux-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_VISPER_Redux : NSObject
+@end
+@implementation PodsDummy_VISPER_Redux
+@end
